fe670d527f3dbc0d7c9080c0f8f3e2256a0ae2f4
Raul Lopes <raul.dev@outlook.com.br>
2025-07-14 19:24:31 -0400

    feat: implement filter functionality in Home component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -5,7 +5,7 @@ import { SearchInput } from "@/components/Search";
 import { FilterStatus } from "@/types/FilterStatus";
 import { Filter } from "@/components/Filter";
 import { Card } from "@/components/Card";
-
+import { useState } from "react";
 // Aqui definimos os status de filtro disponíveis
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
@@ -22,13 +22,19 @@ const sections = [
   },
 ];
 export function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
   return (
     <View style={styles.container}>
       <Header title="ToDo List" showButton />
       <SearchInput />
       <View style={styles.filterContainer}>
         {FILTER_STATUS.map((status) => (
-          <Filter key={status} status={status} isActive />
+          <Filter 
+          key={status}
+           status={status} 
+           isActive={status == filter}
+           onPress={() => setFilter(status)}
+           />
         ))}
         <TouchableOpacity style={styles.clearButton}>
           <Text style={styles.clearButtonText}>Limpar</Text>
